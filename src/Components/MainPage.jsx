2ae905c200ec4a863a2d98f95c2e480c0ae08cf3
Jacob Ankney <Jacob@Jacobs-MacBook-Pro.local>
2019-06-18 16:32:03 -0600

    frontend started. login toggle works

--- src/Components/MainPage.jsx
+++ src/Components/MainPage.jsx
@@ -1,12 +1,32 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import {Link} from 'react-router-dom';
 
 
 
 export default class MainPage extends Component {
-  render(){
-    return(
-      <div>
-        Main
+  render() {
+    return (
+      <div className={`mainPage`}>
+
+        <div className="headline">
+
+
+          <h1>Welcome to Ambiverb</h1>
+          <h2>The place where you come to get your gear</h2>
+          <input type="text" className="searchBar" placeholder="Find Gear" />
+
+        <ul>
+
+          <li>Electric Guitars</li>
+          <li>Synths</li>
+          <li>Bass Guitars</li>
+          <li>Vintage</li>
+        </ul>
+
+        </div>
+
+      
+
       </div>
     )
   }
--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,12 +1,82 @@
-import React, {Component} from 'react'
-
+import React, { Component } from 'react'
+import axios from 'axios'
 
 export default class NavBar extends Component {
-  render(){
-    return(
-      <div>
-        Navbar
-      </div>
-    )
+
+  constructor() {
+    super()
+    this.state = {
+      email: '',
+      pass: '',
+      isLoggedIn: false,
+      showLogin: false
+    }
+  }
+
+  changeHandler = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+
+  loginHandler = () => {
+    axios.post('/auth/login', this.state).then((res) => {
+      if (res.data.user.isLoggedIn) {
+        this.setState({
+          isLoggedIn: res.data.user.isLoggedIn,
+          email:'', 
+          pass:'', 
+          showLogin:false
+
+        })
+      }
+    })
+  }
+
+logoutHandler= ()=> {
+  axios.get('/auth/logout').then(res=> {
+    if(res.status===200){
+      this.setState({
+        isLoggedIn:false
+      })
+    }
+  })
+}
+
+toggleLogin=()=> {
+  this.setState({
+    showLogin:!this.state.showLogin
+  })
+
+
+}
+
+  render() {
+    if (!this.state.isLoggedIn) {
+
+
+
+      return (
+        <div>
+<button onClick={this.toggleLogin}>Login</button>
+
+<div className={` loginForm ${this.state.showLogin ? '': "hidden"}`}>
+<input type="text" name='email' required placeholder="email" onChange={this.changeHandler} />
+
+<input type="password" name='pass' required placeholder="password" onChange={this.changeHandler} />
+
+<button onClick={this.loginHandler}>Login</button>
+</div>
+
+        </div>
+      )
+    } else {
+      return (
+        <div>
+          <button onClick={this.logoutHandler}>Logout</button>
+        </div>
+      )
+    }
+
   }
 }
