6c3f4b689265c2df82510034347d006b685ef8ad
Jacob Ankney <Jacob@Jacobs-MacBook-Pro.local>
2019-06-19 15:08:51 -0600

    started on listing component

---
+++ src/Components/Listing.jsx
@@ -0,0 +1,21 @@
+import React, {Component} from 'react'
+import axios from 'axios'
+import { connect } from 'react-redux';
+import {Link} from 'react-router-dom'
+
+
+
+
+
+
+const mapStateToProps=(state)=> {
+  return {
+
+  }
+}
+
+const mapDispatchToProps= {
+
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Listing)
